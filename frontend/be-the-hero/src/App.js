8d5f445913e77fb9f3a549c95bebc05aa48e588f
gustavisual <gustafx.suporte@gmail.com>
2020-04-10 05:47:11 -0300

    Primeiro commit com interface

---
+++ frontend/be-the-hero/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './global.css';
+import Routes from './routes';
+
+/**
+ * JSX - Javascript XML
+ * 
+ * No React um componente nada mais é que uma função retornando um HTML.
+ */
+
+function App() {
+  return (
+    <div>
+      <Routes /> 
+    </div>
+  );
+}
+ 
+export default App;
